d45f32503f1d39f2fae3d6dc474e11159fbed272
dybucc <149513579+dybucc@users.noreply.github.com>
2026-02-24 19:03:02 +0100

    chore: wip

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -8,6 +8,29 @@ fn main() {
         .map(|e| e.parse::<isize>().unwrap())
         .collect::<Vec<_>>()[..]
     else {
-        return;
+        panic!();
     };
+    let point_in_b = 1. / y as f64;
+    println!(
+        "{}",
+        if x.is_positive() {
+            let mut state_a = 0.;
+            let mut state_b = 0_isize;
+            while ((state_b - 1) as f64) < state_a - point_in_b {
+                state_a -= point_in_b;
+                state_b -= 1;
+            }
+            format!("{}", state_a.midpoint(state_a - point_in_b))
+        } else if x.is_negative() {
+            let mut state_a = 0.;
+            let mut state_b = 0_isize;
+            while ((state_b + 1) as f64) > state_a + point_in_b {
+                state_a += point_in_b;
+                state_b += 1;
+            }
+            format!("{}", state_a.midpoint(state_a + point_in_b))
+        } else {
+            "ALL GOOD".to_string()
+        }
+    )
 }
