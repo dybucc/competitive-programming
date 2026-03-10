fc8bd74dc4366304b7e69f504044f0b1476e221d
dybucc <149513579+dybucc@users.noreply.github.com>
2026-03-10 19:04:08 +0100

    chore: wip

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -1,64 +1,66 @@
-use std::io::{self,
-              Read};
+use std::io::{self, Read};
+
 fn main() {
-  let mut buf = String::new();
-  io::stdin().read_to_string(&mut buf).unwrap();
-  let mut lines = buf.lines();
-  let mut input =
-    Vec::with_capacity(lines.next().unwrap().parse::<usize>().unwrap());
-  let mut accum = ["nil"; 2];
-  for (i, l) in lines.enumerate() {
-    accum[i % 2] = l;
-    if i != 0 && i % 2 != 0 {
-      input.push(accum.map(ToString::to_string));
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut lines = buf.lines();
+    let mut input =
+        Vec::with_capacity(lines.next().unwrap().parse::<usize>().unwrap());
+    let mut accum = ["nil"; 2];
+    for (i, l) in lines.enumerate() {
+        accum[i % 2] = l;
+        if i != 0 && i % 2 != 0 {
+            input.push(accum.map(ToString::to_string));
+        }
     }
-  }
-  // Permute however as many elements of the input string as there are `?`
-  // bytes. This should yield `n` initial states, which already prune the search
-  // space, as only 0s can be made into 1s, and from that point on, the search
-  // for each individual state consists of all states derived from the initial
-  // state where 0s are 1s, and all possible combinations (not permutations) of
-  // the input sequence.
-  //
-  // Thus, for a `k`-long input sequence, where `1 <= k <= 100`, and there are
-  // some `l` amount of `?` bytes, there should be `2^l` initial states, where
-  // `0 <= l <= k`. Each of these ought bind its `move` counter to however as
-  // many `?` bytes there are in the input sequence. Solving by actually
-  // computing the permutation is bonkers, as the cost of this operation is
-  // `Theta(2^l)`, where `0 <= l <= k, 1 <= k <= 100`, worst-case `2^100 =
-  // 1.23e30`. Thus, computing through a complete search filter is infeasible,
-  // and a recursive generator should be used instead.
-  //
-  // A recursive generator is only feasible if the computations can be made in
-  // single-step fashion. The result of `2^l` ought be computed, but the
-  // exhorbitant amount of bytes corresponding with all initial states need not
-  // be stored prior to starting the complete search. Instead, the routine in
-  // charge of recursing should behave in a fashion akin to that of iterator
-  // adapters and short-circuiting iterator consumers; Namely, it should perform
-  // only the computations regarding the "current" permutation result. This
-  // would allow avoiding an immediate error regarding memory allocation limits,
-  // as the worst case scenario would still only ever hold memory for a single
-  // one of the `2^l` initial states, and its subsequent recursive stack frame
-  // allocations.
-  //
-  // The above scheme would, in turn, require identifying the "current"
-  // permutation. Because all initial states to which the input sequence
-  // resolves to consist purely of `k`-element radix 2-encoded number sequences,
-  // one can enumerate the permutations in terms of the decimal number to which
-  // they map if the number were converted to base 10. For the bounds on `l`,
-  // this would require a type capable of holding at least 100 bits. In Rust,
-  // there exists only one such type, `{u,i}128`.
-  //
-  // A single instance of one such number would allow computing upwards of 100
-  // elements, and thus would allow
-  //
-  // 10??01 -> 100001; ...
-  //        -> 100101; ...
-  //        -> 101001; ...
-  //        -> 101101; ...
-  // target: 100111
-  for (i, [a, b]) in input.into_iter().enumerate() {
-    for (i, b) in a.bytes().enumerate() {}
-    println!("Case {}: {}", i + 1, -1);
-  }
+    // For any input sequence `s`, for some `l` number of `?` bytes in `s`,
+    // there exists `2^l` initial states from which to possibly derive some
+    // output sequence `s'`, such that `s' = w`, for some sequence `w` where
+    // `|w| = `s``.
+    //
+    // Furhter, the bounds on `|s|`, which from now on we shall refer to as `k`,
+    // are such that for `1 <= k <= 100`, `0 <= l <= k`, which implies an upper
+    // bound on the number of initial states of `2^l`, for `l = 100`, `2^(100) =
+    // 1.23e30`.
+    //
+    // Thus, computing all initial states through a complete search filter is
+    // infeasible, and one should instead attempt to solve the problem in terms
+    // of a recursive complete search generator that pruned the search space
+    // in-place.
+    //
+    // A possible alternative is to take care of the number that the binary
+    // encoding provides, and prune the search space much earlier. This would
+    // encompass taking into consideration the target sequence `w`, and
+    // computing the integer value of the number denoted by it, `theta` from
+    // here onwards. Then, one would have to compare `theta` with the input
+    // sequence and resolve as the _single_ initial state, the largest number
+    // smaller than or equal to `theta`. We will leave the details of that
+    // procedure for later. Upon resolving an initial state, where no more moves
+    // involving a replacement of `?` bytes is required, the algorithm should
+    // enumerate all numbers between the one denoted by the initial state, from
+    // here on referred to as `omega`, and the target sequence's numeric value,
+    // namely `theta`. If any one of the resulting sequences, parsed from
+    // computing any one of the above, matches a sequence of move operations
+    // that forms a path between the initial state and the target sequence, then
+    // a solution is feasible. Counting the number of steps in the path yields
+    // the solution.
+    //
+    // The above formulation, even if unproven, does seem feasible, and
+    // reduces the search space the moment it determines the largest number
+    // smaller than or equal to the target sequence's representation.
+    // Additionally, the final steps involving finding a path seem to fit nicely
+    // with a graph problem, with as many vertices as elements in the initial
+    // solution.
+    //
+    // 10??01 -> 100001; ...
+    //        -> 100101; ...
+    //        -> 101001; ...
+    //        -> 101101; ...
+    // Test target 1: 100111
+    // Test target 2: 000111
+    // Test target 3: 110111
+    for (i, [a, b]) in input.into_iter().enumerate() {
+        for (i, b) in a.bytes().enumerate() {}
+        println!("Case {}: {}", i + 1, -1);
+    }
 }
