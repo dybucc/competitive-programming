3065f5fc7e828134d6710917d8b62a1ec41713a9
dybucc <149513579+dybucc@users.noreply.github.com>
2026-04-02 19:02:29 +0200

    chore: wip

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -7,8 +7,8 @@ use std::{
 fn main() {
   let mut buf = String::new();
   io::stdin().read_to_string(&mut buf).unwrap();
-  let mut lines = buf.lines();
-  let (mut a, mut b) = (HashSet::with_capacity(9), HashSet::with_capacity(9));
+  let (mut lines, mut a, mut b) =
+    (buf.lines(), HashSet::with_capacity(9), HashSet::with_capacity(9));
   (0..lines.next().unwrap().parse::<usize>().unwrap()).for_each(|_| {
     match (0..3).try_for_each(|i| {
       lines.next().unwrap().bytes().enumerate().try_for_each(
@@ -31,18 +31,18 @@ fn main() {
               match c.iter().enumerate().fold(
                 ([false; 3], [false; 3]),
                 |(mut a_c, mut b_c), (i, c)| match i {
-                  | 0..3 => (
+                  | ..3 => (
                     (a_c[i] = a.contains(c), a_c).1,
                     (b_c[i] = b.contains(c), b_c).1,
                   ),
                   | _ => unreachable!(),
                 },
               ) {
-                | ([true, true, true], [false, false, false]) =>
+                | (a, b) if a.iter().zip(b).all(|(&a, b)| a && !b) =>
                   (a_wins += 1, (a_wins, b_wins)),
-                | ([false, false, false], [true, true, true]) =>
+                | (a, b) if a.iter().zip(b).all(|(&a, b)| !a && b) =>
                   (b_wins += 1, (a_wins, b_wins)),
-                | ([false, false, false], [false, false, false]) =>
+                | (a, b) if a.iter().zip(b).all(|(&a, b)| !(a && b)) =>
                   ((), (a_wins, b_wins)),
                 | _ => return ControlFlow::Break(()),
               }
@@ -53,9 +53,8 @@ fn main() {
           | ControlFlow::Continue((1, 0)) if a.len() == b.len() + 1 => "yes",
           | ControlFlow::Continue((0, 1)) if a.len() == b.len() => "yes",
           | ControlFlow::Continue((0, 0))
-            if matches!(
-              a.len(), n if n == b.len() || n == b.len() + 1
-            ) =>
+            if matches!(a.len(),
+              n if n == b.len() || n == b.len() + 1) =>
             "yes",
           | _ => "no",
         }),
